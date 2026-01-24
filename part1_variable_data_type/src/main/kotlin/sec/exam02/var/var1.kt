6336ef9215d88df46618ed82bd1c2d5cafda6dee
JEONG-BD <dev.byeongdu@gmail.com>
2026-01-25 00:40:41 +0900

    var val의 차이

---
+++ part1_variable_data_type/src/main/kotlin/sec/exam02/var/var1.kt
@@ -0,0 +1,15 @@
+package sec.exam02.`var`
+
+fun main(args: Array<String>) {
+    //
+
+    val number = 10
+    var lan: Int = 1
+    val secondNumber : Int = 11
+    println("number = ${number}")
+    println("secondNumber = ${secondNumber}")
+    println("lan = ${lan}")
+
+    //number = 20 // error
+
+}
